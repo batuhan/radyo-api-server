d09b63facef5fce03a8ed659395268f2a020a90d
Batuhan Icoz <batuhanicoz@gmail.com>
2011-08-16 14:26:20 +0300

    added support for icecast
    well, again, not exactly.

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,8 @@
 <?php
 
 $serverinformation = array(
-    'version' => 1, // this one is for future
+    'type'    => 'shoutcast', // for future versions. currently only shoutcast1 supported.
+    'version' => 1, // this one is also for future versions
     'host'    => 'yourserver', // FQDN or IP. I normally prefer IP.
     'port'    => 8000, // 8000 is the default but nobody uses it.
     'password'=> 'thisisapasswordandimtheonlyonewhoknowsit' // this is your admin password NOT broadcaster password!!11
