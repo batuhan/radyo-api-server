04ea4e5bcc850c07f2e747465f6fc6c95b4b5e17
Batuhan Icoz <batuhanicoz@gmail.com>
2011-08-16 16:15:13 +0300

    old api is gone. i'll open a new repo for it.

--- old/web/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require '/srv/private/frameworks/f3/lib/base.php'; //catiyi yukle
-
-require_once '../veriler/bilgiler.php'; //gereken bilgileri çek
-
-include '../veriler/genel.php'; //apiyi yükle
-
-F3::run();
