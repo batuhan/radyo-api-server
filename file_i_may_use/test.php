016d7ce1435e511d48c04250b9cecf549f355239
Batuhan Icoz <batuhanicoz@gmail.com>
2011-09-17 02:18:02 +0300

    0

--- file_i_may_use/test.php
+++ file_i_may_use/test.php
@@ -31,25 +31,25 @@ function get_contents($ip, $port){
 	include '../vendor/simple_html_dom.php';
 	
 	$html = str_get_html(curl_get_contents('http://'.$ip.':'.$port.'/index.html'));
-	foreach($html->find('td font b') as $element) $contents[] = $element->plaintext;
+	foreach($html->find('td font b') as $element){ $contents[] = $element->plaintext;}
 
 	if($contents[0] === 'Server is currently up and public.'){
 		
-		preg_match_all ('/[0-9]+(?:\.[0-9]*)?/', $contents[1], $numbers[1]);
-		preg_match_all ('/[0-9]+(?:\.[0-9]*)?/', $contents[2], $numbers[2]);
+		preg_match_all ('/[0-9]+(?:\.[0-9]*)?/', $contents[1], $numbers_one);
+		preg_match_all ('/[0-9]+(?:\.[0-9]*)?/', $contents[2], $numbers_two);
 
 		$final_contents = array(
 			'status' => TRUE,
 			'listeners' => array(
-				'total' => $numbers[2][0],
-				'unique' => $numbers[2][2],
+				'total' => $numbers_two[0],
+				'unique' => $numbers_two[2],
 				'peak' => $contents[3],
 				'max' => $numbers[2][1],
 				'average_listen_time' => $contents[4]
 			),
 			'title' => $contents[5],
 			'type' => $contents[6],
-			'bitrate' => $numbers[1][0].'kbps',
+			'bitrate' => $numbers_one.'kbps',
 			'genre' => $contents[7],
 			'url' => $contents[8],
 			'icq' => $contents[9],
